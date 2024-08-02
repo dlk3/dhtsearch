1a2c2e51741275f671e9da25565d4d1e27dc9b62
Dave King <dave@daveking.com>
2024-08-02 14:11:04 +0000

    Setup to begin conversion to docker cluster on new server

--- www-php/settings.php
+++ www-php/settings.php
@@ -10,8 +10,8 @@ $settings = [
 			'name' => 'DHTSearch',
 			'short' => 'DS',
 			'lead' => 'A BitTorrent DHT Search Engine',
-			'domain' => 's34rch3r.duckdns.org',
-			'url' => 'https://s34rch3r.duckdns.org',
+			'domain' => 'dhtsearch.daveking.com',
+			'url' => 'https://dhtsearch.daveking.com',
 			'version' => '0.0.8',
 			'contact' => 'dhtsearch.0@gmail.com'
 		],
